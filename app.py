b42a31c3c6c8c99d60c70f1124370c66061a8afb
vinifranco48 <vinicius.franco@alu.unibalsas.edu.br>
2024-12-05 10:07:41 -0300

    feat:

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def connect_to_gsheet(sheet_name):
         credentials_info = {
             "type": "service_account",
             "project_id": os.getenv('PROJECT_ID'),
-            "private_key_id": os.getenv('PRIVATE_KEY_ID').replace('\\n', '\n'),
+            "private_key_id": os.getenv('PRIVATE_KEY_ID'),
             'private_key': os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
             "client_email": os.getenv('CLIENT_EMAIL'),
             "client_id": os.getenv('CLIENT_ID'),  # Corrigido para usar CLIENT_ID corretamente
